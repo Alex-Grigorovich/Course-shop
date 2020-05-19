5c7f66abc1f546325383afeb0687415a45b765c9
Alex-Grigorovich <holdencolfield37@gmail.com>
2020-05-19 22:59:59 +0300

    Add : Navbar, routes, form and primitive submit form

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-var http = require("http");
+const express = require('express')
+const app = express();
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
-http.createServer(function (request, response) {
+//инициализация порта
+const PORT = process.env.PORT || 3000;
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
 
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
-    console.log('asdsa');
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
-    response.end();
-}).listen(3000);
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+
+//получение страницы
+
+
+app.listen(PORT, () => {
+    console.log(`SERVER STARTED ON PORT ${PORT}`)
+
+})
