da49c37b3d11cac2517fca5053cebed23c0fff65
Alex-Grigorovich <holdencolfield37@gmail.com>
2020-05-26 23:30:17 +0300

    add button "delete" from Courses, first User and prototype Cart

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const addRoutes = require('./routes/add')
 const cartRoutes = require('./routes/cart')
 const coursesRoutes = require('./routes/courses')
 const mongoose = require('mongoose')
+const User = require('./models/user')
 
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 
 
 //инициализация порта
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const exphbs = require('express-handlebars')
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -24,6 +25,17 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next)=>{
+    try{
+        const user = await User.findById('5ecd6a50c2bbc340c3fe743b')
+        req.user = user
+        next()
+    }catch (e) {
+        console.log(e)
+    }
+
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
@@ -33,13 +45,23 @@ app.use('/courses', coursesRoutes)
 
 //получение страницы
 
-//pass = HlL94Lsde7f51C4x
+//pass = HlL94Lsde7f51C4x  52m5a37jDtUDdugn
 
 async function start() {
     try {
-        const url = `mongodb+srv://Dan:HlL94Lsde7f51C4x@cluster0-u6m3d.mongodb.net/myshop`
+        const url = `mongodb+srv://Donn:52m5a37jDtUDdugn@cluster0-u6m3d.mongodb.net/myshop`
         await mongoose.connect(url, {useNewUrlParser: true,
-            useUnifiedTopology: true})
+            useUnifiedTopology: true,
+        useFindAndModify:false})
+        const candidate =await User.findOne()
+        if(!candidate){
+            const user = new User({
+                email:'holdencolfield37@gmail.com',
+                name:'Oleg',
+                cart:{items:[]}
+            })
+            await user.save()
+        }
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`)
         })
--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,10 @@
 const {Router} = require('express')
-const Cart = require('../models/cart')
 const Course = require('../models/course')
 const router = Router()
 
-
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Cart.add(course)
+    const course = await Course.findById(req.body.id)
+    await req.user.addToCart(course)
     res.redirect('/cart')
 })
 
@@ -16,14 +14,14 @@ router.delete('/remove/:id', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-    const cart = await Cart.fetch()
-    res.render('cart', {
-        title: 'Корзина',
-        isCart: true,
-        courses: cart.courses,
-        price: cart.price
-    })
+    // const card = await Card.fetch()
+    // res.render('card', {
+    //   title: 'Корзина',
+    //   isCard: true,
+    //   courses: card.courses,
+    //   price: card.price
+    // })
+    res.json({test: true})
 })
-
 
 module.exports = router
