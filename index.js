221402ae3e500954bf3427bd7c8638e771b865d8
Alex-Grigorovich <holdencolfield37@gmail.com>
2020-05-27 23:03:30 +0300

    Add cart and order

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const cartRoutes = require('./routes/cart')
 const coursesRoutes = require('./routes/courses')
+const ordersRoutes = require('./routes/orders')
 const mongoose = require('mongoose')
 const User = require('./models/user')
 
@@ -42,6 +43,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/cart', cartRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/orders', ordersRoutes)
 
 //получение страницы
 
--- models/user.js
+++ models/user.js
@@ -27,7 +27,6 @@ const userSchema = new Schema({
     }
 })
 
-
 userSchema.methods.addToCart = function(course) {
     const items = [...this.cart.items]
     const idx = items.findIndex(c => {
@@ -42,11 +41,26 @@ userSchema.methods.addToCart = function(course) {
             count: 1
         })
     }
+    this.cart = {items}
+    return this.save()
+}
 
-    // const newCart = {items: clonedItems}
-    // this.cart = newCart
+userSchema.methods.removeFromCart = function(id){
+    let items = [...this.cart.items]
+    const idx = items.findIndex(c => c.courseId.toString() === id.toString())
+
+    if (items[idx].count === 1) {
+        items = items.filter(c => c.courseId.toString() !== id.toString())
+    } else {
+        items[idx].count--
+    }
 
     this.cart = {items}
+    return this.save()
+}
+
+userSchema.methods.clearCart= function(){
+    this.cart = {items:[]}
     return this.save()
 }
 
